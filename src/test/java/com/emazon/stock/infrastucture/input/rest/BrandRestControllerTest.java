ae38941cf7987d0409471a2d065eef46a7009faf
ortizjulian <julian.ortixs@gmail.com>
2024-08-25 19:54:06 -0500

    Create Unit Test for BrandUseCase and BrandRestController

---
+++ src/test/java/com/emazon/stock/infrastucture/input/rest/BrandRestControllerTest.java
@@ -0,0 +1,91 @@
+package com.emazon.stock.infrastucture.input.rest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import com.emazon.stock.application.dto.BrandDto;
+import com.emazon.stock.application.handler.IBrandHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(controllers = BrandRestController.class)
+@ExtendWith(MockitoExtension.class)
+class BrandRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private IBrandHandler iBrandHandler;
+
+
+    @Test
+    void BrandRestController_SaveBrand_ShouldReturnCreatedStatus() throws Exception {
+        BrandDto brandDto = new BrandDto("Mattelsa", "Marca colombiana");
+
+        ResultActions response = mockMvc.perform(post("/brand")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(brandDto)));
+
+        response.andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    void BrandRestController_SaveBrand_WhenNameIsNull_ShouldReturnBadRequest() throws Exception {
+        BrandDto invalidBrand = new BrandDto( null, "Ropa de marca colombiana");
+
+        ResultActions response = mockMvc.perform(post("/brand")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(invalidBrand)));
+
+        response.andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void BrandRestController_SaveBrand_WhenDescriptionIsNull_ShouldReturnBadRequest() throws Exception {
+        BrandDto invalidBrand = new BrandDto( "Mattelsa",null);
+
+        ResultActions response = mockMvc.perform(post("/brand")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(invalidBrand)));
+
+        response.andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void BrandRestController_SaveBrand_WhenNameExceeds50Characters_ShouldReturnBadRequest() throws Exception {
+
+        BrandDto invalidBrand = new BrandDto(
+                "Este nombre de marca es demasiado largo para probar que la Excepción MethodArgumentNotValidException sea lanzada",
+                "Ropa de marca colombiana");
+
+        ResultActions response = mockMvc.perform(post("/brand")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(invalidBrand)));
+
+        response.andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+    @Test
+    void BrandRestController_SaveBrand_WhenDescriptionExceeds120Characters_ShouldReturnBadRequest() throws Exception {
+
+        BrandDto invalidBrand = new BrandDto(
+                "Mattelsa",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec laoreet euismod neque vitae eleifend. Nam tempus, ipsum id suscipit faucibus, ante ligula luctus sem, eu dignissim nulla justo vitae tellus. Fusce ut bibendum erat, id porttitor nunc. Cras molestie neque et libero ornare fermentum.");
+
+        ResultActions response = mockMvc.perform(post("/brand")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(invalidBrand)));
+
+        response.andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+}
